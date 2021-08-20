311b5d288693b6439251660fdc5f44d9ab1f42ca
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2021-08-20 14:03:03 +0530

    closing #2

--- Extracting_file_name/file.c
+++ Extracting_file_name/file.c
@@ -1,94 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node
-{
-    char data;
-    struct Node *next;
-}*head;
-
-struct Node *helper(char input[])
-{
-    for (int i = strlen(input) - 1; i >= 0; i--)
-    {
-
-        if (head == NULL)
-        {
-            head = (struct Node *)malloc(sizeof(struct Node));
-            head->data = input[i];
-            head->next = NULL;
-        }
-        else
-        {
-            if (input[i] == '/')
-            {
-                break;
-            }
-            else
-            {
-                struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
-                new_node->data = input[i];
-                new_node->next = head;
-                head = new_node;
-            }
-        }
-    }
-    return head;
-}
-
-// char *helper(char input[]){
-//     char *p;
-//     p = calloc(strlen(input), sizeof(int));
-
-//     if (p == NULL)
-//     {
-//         printf("memory not allocated");
-//     }
-//     else
-//     {
-//         for (int i = strlen(input)- 2; i >= 0; i--)
-//         {
-//             p[i]=input[i];
-//             char c =p[i];
-//             int x=0;
-//             // if (input[i] == '/')
-//             // {
-//             //     i++;
-
-//             //     for (int j = 0; j < strlen(input); j++)
-//             //     {
-
-//             //         if (input[i] == '\0')
-//             //         {
-//             //             break;
-//             //         }
-//             //         else
-//             //         {
-//             //             p[j] = input[i];
-//             //         }
-//             //         i++;
-//             //     }
-//             //     break;
-//             // }
-//         }
-//     }
-//    return p;
-// }
-
-
 int main()
 {
 
- char input[] = "C:/Users/Public/123124grdfggfdf";
-   struct Node *p=helper(input);
-    // for (; *p; p++)
-    //     printf("%c", *p);
-    while (p != NULL)
+    char *path = "ab/cde/fg.out";
+
+    for (int i = strlen(path) - 1; i >= 0; i--)
     {
-        printf("%c", p->data);
-        p = p->next;
+        if (path[i] == '/')
+        {
+            path = &path[i + 1];
+            break;
+        }
     }
+
     return 0;
 }
-
-
