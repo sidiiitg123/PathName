1cbc9c94182a353e809b7ab1e01e938fa3c1fe6a
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2021-08-20 11:46:30 +0530

    modified #1

--- Extracting_file_name/file.c
+++ Extracting_file_name/file.c
@@ -1,51 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *helper(char input[]){
-    char *p;
-    p = calloc(strlen(input), sizeof(int));
+struct Node
+{
+    char data;
+    struct Node *next;
+}*head;
 
-    if (p == NULL)
+struct Node *helper(char input[])
+{
+    for (int i = strlen(input) - 1; i >= 0; i--)
     {
-        printf("memory not allocated");
-    }
-    else
-    {
-        for (int i = strlen(input)- 1; i >= 0; i--)
+
+        if (head == NULL)
+        {
+            head = (struct Node *)malloc(sizeof(struct Node));
+            head->data = input[i];
+            head->next = NULL;
+        }
+        else
         {
             if (input[i] == '/')
             {
-                i++;
-
-                for (int j = 0; j < strlen(input); j++)
-                {
-
-                    if (input[i] == '\0')
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        p[j] = input[i];
-                    }
-                    i++;
-                }
                 break;
+            }
+            else
+            {
+                struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+                new_node->data = input[i];
+                new_node->next = head;
+                head = new_node;
             }
         }
     }
-   return p;
+    return head;
 }
+
+// char *helper(char input[]){
+//     char *p;
+//     p = calloc(strlen(input), sizeof(int));
+
+//     if (p == NULL)
+//     {
+//         printf("memory not allocated");
+//     }
+//     else
+//     {
+//         for (int i = strlen(input)- 2; i >= 0; i--)
+//         {
+//             p[i]=input[i];
+//             char c =p[i];
+//             int x=0;
+//             // if (input[i] == '/')
+//             // {
+//             //     i++;
+
+//             //     for (int j = 0; j < strlen(input); j++)
+//             //     {
+
+//             //         if (input[i] == '\0')
+//             //         {
+//             //             break;
+//             //         }
+//             //         else
+//             //         {
+//             //             p[j] = input[i];
+//             //         }
+//             //         i++;
+//             //     }
+//             //     break;
+//             // }
+//         }
+//     }
+//    return p;
+// }
 
 
 int main()
 {
 
  char input[] = "C:/Users/Public/123124grdfggfdf";
-    char *p=helper(input);
-    for (; *p; p++)
-        printf("%c", *p);
-   
+   struct Node *p=helper(input);
+    // for (; *p; p++)
+    //     printf("%c", *p);
+    while (p != NULL)
+    {
+        printf("%c", p->data);
+        p = p->next;
+    }
     return 0;
 }
 
